09be647a24c1b425936607bf626e8d7c7dec3ddc
Dsux Zhou <xDsux@sina.com>
2018-01-26 01:00:44 +0800

    practice for day1
    test

---
+++ python/Day1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# practice for input and output
+# print("please input your name.")
+# name = input()
+# print("your name is", name)
+# name2 = 'day2'
+# name2
+
+#test '\'
+# print('I\'m dsux')
+
+# 测试中文和%
+# print('%s, %f < %d' % ('我说', 7.23, 8))
+
+# test list antuple
+# lista = ['a', 'b', 'c']
+# print(len(lista))
+# print(lista[-1])
+# lista.pop()
+# lista.pop(0)
+# lista[0] = 'd'
+# lista.append('e')
+# lista.insert(1, 'f')
+# print(lista)
+
+# tpa = ()
+# print('a:', tpa)
+# tpb=(1, 2)
+# print(tpb)
+
+# test for in | while
+# tpa = (1, 2, 3)
+# b = 0
+# c = 3
+# for a in tpa:
+    # b = b + 1
+    # print(b)
+# print("run over")
+
+# while c > 0:
+    # c = c - 1
+    # print(tpa[c])
+# print('while over')
+
+# test for dict
+d = {'xiaoming' : 99, 'xiaomei' : 'youxiu'}
+print(d['xiaoming'])
+print(d['xiaomei'])
+print(d['a'])
