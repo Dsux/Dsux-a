c30aebdd846a9aabdefd3a353e2a9a07d5ddd254
Dsux Zhou <xDsux@sina.com>
2018-01-26 22:22:31 +0800

    practice for day2

---
+++ python/Day2.py
@@ -0,0 +1,39 @@
+# test funciton
+# a = abs
+# print(a(-1))
+# def myFunc(x):
+    # if x >= 1:
+        # return 1
+    # else:
+        # return 0
+# print(myFunc(33))
+
+# print(myFunc(int('a'))) 
+
+# a = 'x'
+# def changea():
+    # a = 'y'
+# changea()
+# print(a)
+
+# test tuple
+# x,y,z = (1, 2, 3)
+# print(y)
+
+#test function
+# def changeablePara(*numbers):
+    # num = 0
+    # for n in numbers:
+        # num = num + n
+    # return num
+# tpa = (1, 2, 3, 4)
+# print(changeablePara(*tpa))
+
+# a = [1, 2, [3, 4]]
+# print(a)
+
+print(range(10))
+la = list(range(5))
+print(la)
+la = la[-3 : ]
+print(la)
