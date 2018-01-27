d5432d3a108c60e5e4bcccede34dccff67a25492
Dsux Zhou <xDsux@sina.com>
2018-01-27 23:54:04 +0800

    practice for day3

---
+++ python/Day3.py
@@ -0,0 +1,9 @@
+# day3
+# print([x*x for x in range(1, 11) if x % 3 == 0])
+# d = {'a' : 'vr', 'b' : 'vx', 'c' : 'vb'}
+# for k, v in d.items():
+    # print(k, '=', v)
+# L1 = ['Hello', 'World', 18, 'Apple', None]
+a = [1,2]
+b = [2,3,4]
+print(a + b)
